0829112dcd0d7d5d48c73f0468b3148dbd3a0295
saeed144 <saaed.amini@gmail.com>
2023-10-21 23:00:28 +0330

    make archive pages

---
+++ archive.php
@@ -0,0 +1,32 @@
+<?php
+    get_header();
+?>
+<section class="titlesection ">
+    <div class="">
+        <div class="container">
+            <h1><?php the_archive_title();?></h1>
+            <div>
+                <!-- <span><?php  the_time( 'F j, Y' ); ?> </span>
+                <span><?php  the_tags("<span> The tags: " , " </span>"); ?></span>
+                <span><?php  the_category(); ?></span>
+                <span><?php  comments_number() ?></span> -->
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <?php
+            if(have_posts(  )){
+                while(have_posts(  )){
+                    the_post(  );
+                    // the_content(  );
+                    get_template_part( 'template-parts/content' , 'archive' );
+                }
+            }
+        ?>
+    </div>
+</section>
+<?php
+    get_footer();
+?>

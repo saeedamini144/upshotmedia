8a3922f8f4922bc3aa2ba711b2f243a41da8107e
saeed144 <saaed.amini@gmail.com>
2023-10-21 22:14:50 +0330

    add post image

--- single.php
+++ single.php
@@ -15,12 +15,12 @@
     </div>
 </section>
 <div class="container">
+    <div class="m-5"><?php the_post_thumbnail();?></div>
     <?php
         if(have_posts(  )){
             while(have_posts(  )){
                 the_post(  );
                 // the_content(  );
-                // the_post_thumbnail(  );
                 get_template_part( 'template-parts/content' , 'article' );
             }
         }

3bc9c7d1f48abac684075b7ea39d8b9936684b15
saeedamini <saaed.amini@gmail.com>
2024-01-19 02:15:15 +0330

    add search style and resault

--- search.php
+++ search.php
@@ -1,30 +1,36 @@
 <?php
-    get_header();
+get_header();
 ?>
-<section class="titlesection ">
+<section class="titlesection mb-5 ">
     <div class="">
         <div class="container">
-            <h1>Search Resault</h1>
-            <div>
-                <!-- <span><?php  the_time( 'F j, Y' ); ?> </span>
-                <span><?php  the_tags("<span> The tags: " , " </span>"); ?></span>
-                <span><?php  the_category(); ?></span>
-                <span><?php  comments_number() ?></span> -->
-            </div>
+            <h1>Search Resault: <?php the_search_query(); ?></h1>
         </div>
     </div>
 </section>
-<section class="container">
+<section>
+    <div class="container">
+        <div class="row">
             <?php
-                if(have_posts(  )){
-                    while(have_posts(  )){
-                        the_post(  );
-                        // the_content(  );
-                        get_template_part( 'template-parts/content' , 'archive' );
-                    }
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post();
+                    get_template_part('template-parts/content', 'archive');
                 }
+            }
             ?>
+
+        </div>
+    </div>
+
+    <?php
+    the_posts_pagination(array(
+        'mid_size' => 2,
+        'prev_text' => __('صفحه قبلی', 'textdomain'),
+        'next_text' => __('صفحه بعدی', 'textdomain')
+    ));
+    ?>
 </section>
 <?php
-    get_footer();
+get_footer();
 ?>
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form id="searchform" action="<?php echo home_url('/');?>" method="get">
-    <input type="search" id="search" name="search">
+    <input type="search" id="search" name="search" placeholder="<?php the_search_query(); ?>">
     <input type="submit" id="searchSubmit" value="Search">
 </form>

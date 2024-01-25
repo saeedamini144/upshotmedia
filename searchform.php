fb61465609b178d9e600a15db7e0f03555713165
saeedamini <saaed.amini@gmail.com>
2024-01-25 23:19:31 +0330

    Correct The Search form style and file name

--- searchform.php
+++ searchform.php
@@ -4,6 +4,6 @@ Template Name: Search Page
 */
 ?>
 <form id="searchform" action="<?php echo home_url('/');?>" method="get">
-    <input type="search" class="Search" name="search" placeholder="<?php the_search_query(); ?>">
+    <input type="search" class="Search" name="s" placeholder="<?php the_search_query(); ?>">
     <input type="submit" class="SearchSubmit" value="Search">
 </form>

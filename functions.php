d27049c295e2f09bf15a94d0d1aafc128df3063b
saeedamini <saaed.amini@gmail.com>
2024-02-15 23:05:53 +0330

    Usine Define

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
+define('Address',get_template_directory_uri());//usse define instead of using long atribute
     function upshotmedia_theme_support(){
          // adds dynamic title tag support
         add_theme_support( 'title-tag' );
-        add_theme_support( 'custom-logo' );
-        add_theme_support( 'post-thumbnails' );
+        add_theme_support( 'custom-logo' );//dynamic logo
+        add_theme_support( 'post-thumbnails' );//show dynamic title
     }
     add_action( "after_setup_theme" , "upshotmedia_theme_support");
     
@@ -16,26 +17,34 @@
 
     function upshotmedia_register_style(){
         $version = wp_get_theme()->get("version");
-        wp_enqueue_style('upshotmedia-style', get_template_directory_uri() . '/style.css' , array("upshotmedia-bootstrap") , $version , "all");
-        wp_enqueue_style('upshotmedia-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.rtl.min.css' , array() , "1.0" , "all");
-        wp_enqueue_style('upshotmedia-fontawsome', get_template_directory_uri() . '/assets/css/fontawesome.min.css' , array() , "1.0" , "all");
-        wp_enqueue_style('upshotmedia-animate', get_template_directory_uri() . '/assets/css/animate.min.css' , array() , "1.0" , "all");
-        wp_enqueue_style('upshotmedia-splide', get_template_directory_uri() . '/assets/css/splide-skyblue.min.css' , array() , "1.0" , "all");
+        wp_enqueue_style('upshotmedia-style', Address . '/style.css' , array("upshotmedia-bootstrap") , $version , "all");
+        wp_enqueue_style('upshotmedia-bootstrap', Address . '/assets/css/bootstrap.rtl.min.css' , array() , "1.0" , "all");
+        wp_enqueue_style('upshotmedia-fontawsome', Address . '/assets/css/fontawesome.min.css' , array() , "1.0" , "all");
+        wp_enqueue_style('upshotmedia-animate', Address . '/assets/css/animate.min.css' , array() , "1.0" , "all");
+        wp_enqueue_style('upshotmedia-splide', Address . '/assets/css/splide-skyblue.min.css' , array() , "1.0" , "all");
     }
     add_action( "wp_enqueue_scripts", "upshotmedia_register_style" );
     
     function upshotmedia_register_scripts(){
         wp_enqueue_script('jquery');
         // wp_enqueue_script('upshotmedia-jquery-3.6.0', get_template_directory_uri() . '/assets/javascripts/jquery-3.6.0.js' , array() , "3.6.0" , "all");
-        wp_enqueue_script('upshotmedia-bootstrap.min', get_template_directory_uri() . '/assets/javascripts/bootstrap.min.js' , array('jquery') ,"1.0" , true );
-        wp_enqueue_script('upshotmedia-all.min', get_template_directory_uri() . '/assets/javascripts/all.min.js' , array('jquery') ,"1.0" , true );
-        wp_enqueue_script('upshotmedia-bootstrap.bundle.min', get_template_directory_uri() . '/assets/javascripts/bootstrap.bundle.min.js' , array('jquery') ,"1.0" , true );
-        wp_enqueue_script('upshotmedia-wow.min', get_template_directory_uri() . '/assets/javascripts/wow.min.js' , array('jquery') ,"1.0" , true );
-        wp_enqueue_script('upshotmedia-splide.min', get_template_directory_uri() . '/assets/javascripts/splide.min.js' , array('jquery') ,"1.0" , true );
-        wp_enqueue_script('upshotmedia-mainjs', get_template_directory_uri() . '/assets/javascripts/main.js' , array() ,"1.0" , true );
+        wp_enqueue_script('upshotmedia-bootstrap.min', Address . '/assets/javascripts/bootstrap.min.js' , array('jquery') ,"1.0" , true );
+        wp_enqueue_script('upshotmedia-all.min', Address . '/assets/javascripts/all.min.js' , array('jquery') ,"1.0" , true );
+        wp_enqueue_script('upshotmedia-bootstrap.bundle.min', Address . '/assets/javascripts/bootstrap.bundle.min.js' , array('jquery') ,"1.0" , true );
+        wp_enqueue_script('upshotmedia-wow.min', Address . '/assets/javascripts/wow.min.js' , array('jquery') ,"1.0" , true );
+        wp_enqueue_script('upshotmedia-splide.min', Address . '/assets/javascripts/splide.min.js' , array('jquery') ,"1.0" , true );
+        wp_enqueue_script('upshotmedia-mainjs', Address . '/assets/javascripts/main.js' , array() ,"1.0" , true );
     }
     add_action("wp_enqueue_scripts", "upshotmedia_register_scripts" );
 
+    // custom porfolio post type
+    // function upshotmedia_portfolio(){
+    //      ('Portfolio','array(
+            
+    //     )');
+    // }
+
+    //widegt area
     function upshotmedia_widget_areas(){
         register_sidebar(
             array(

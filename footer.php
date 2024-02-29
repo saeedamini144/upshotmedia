8efe9788e35dc135b0efb4d28f2eb2af17ba97e2
saeedamini <saaed.amini@gmail.com>
2024-02-29 20:28:00 +0330

    CHange footer style

--- footer.php
+++ footer.php
@@ -1,22 +1,23 @@
 <footer class="bg-siah">
-        <div class="container">
-            <div class="row col-12 text-center text-light p-5">
-                <h2><strong>FOLLOW US!</strong></h2>
+    <div class="container">
+        <div class="row col-3  text-light p-5">
+            <!-- <h2><strong>FOLLOW US!</strong></h2>
                 <div class="sociallink">
                     <ul>
-                        <li class="d-inline-block m-2"><span class="fa fa-igloo fa-3x"></span></li>
-                        <li class="d-inline-block m-2" ><span class="fa fa-gift fa-3x"></span></li>
-                        <li class="d-inline-block m-2"><span class="fa fa-gifts fa-3x"></span></li>
+                        <li class="d-inline-block m-2"><span class=" fa-fa-instagram-square fa-3x"></span></li>
+                        <li class="d-inline-block m-2" ><span class=" fa-fa-youtube-square fa-3x"></span></li>
+                        <li class="d-inline-block m-2"><span class="fa-fa-twitter-square fa-3x"></span></li>
                     </ul>
-                </div>
-                <div class="col-12 text-center text-light m-auto p-4">
-                    <p>© Copyright 2020 Upshot Media - All Rights Reserved. View our <a href="#" class="astyle">Privacy and Policy</a></p>
-                </div>
-            </div>
+                </div> -->
         </div>
-    </footer>
-    <?php
-        wp_footer(  );
-    ?>
+    </div>
+    <div class="col-12 text-center text-light m-auto p-4">
+        <p>© Copyright 2020 Upshot Media - All Rights Reserved. View our <a href="#" class="astyle">Privacy and Policy</a></p>
+    </div>
+</footer>
+<?php
+wp_footer();
+?>
 </body>
+
 </html>

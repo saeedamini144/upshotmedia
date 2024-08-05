d31b85cc140ae9e13c4095158412f721ebdfa86b
Saeed Amini <apple@APPLEs-MacBook-Pro.local>
2024-08-05 22:09:43 +0330

    Delet some code

--- archive.php
+++ archive.php
@@ -49,24 +49,3 @@ get_header();
 <?php
 get_footer();
 ?>
-
-<style>
-    .h_iframe-aparat_embed_frame {
-        position: relative;
-    }
-
-    .h_iframe-aparat_embed_frame .ratio {
-        display: block;
-        width: 100%;
-        height: auto;
-    }
-
-    .h_iframe-aparat_embed_frame iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 120px;
-        height: 250px;
-    }
-</style>
-<div class="h_iframe-aparat_embed_frame"><span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/drod404/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe></div>
